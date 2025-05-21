66708f370a9085d5e73234f04d5b0a17a89f51a9
MiguelP <77172963+Miguelzin635@users.noreply.github.com>
2025-05-20 21:57:31 -0300

    Update ex05.py

--- ex05.py
+++ ex05.py
@@ -1,39 +1,44 @@
-total_vendas = []
-total_nomes = []
-total_comissao = []
+todas_vendas = []
+todos_nomes = []
+todas_comissao = []
+total_vendas = 0
+soma = 0
+total = []
+auxiliar = 0
 
-
-for j in range(3):
+for i in range(3):
     nomes = input('Digite o nome do vendedor: ')
-    total_nomes.append(nomes)
+    todos_nomes.append(nomes)
 
 for i in range(3):
-    vendas = float(input(f'Do vendedor {nomes[i]} informe o total das vendas: '))
-    total_vendas.append(vendas)
+    vendas = float(input(f'Do vendedor {todos_nomes[i]} informe o total das vendas: '))
+    todas_vendas.append(vendas)
 
 for i in range(3):
-    comissao = float(input(f'Do vendedor {nomes[i]} informe o percentual de comissão(%): '))
-    total_comissao.append(comissao)
+    comissao = float(input(f'Do vendedor {todos_nomes[i]} informe o percentual de comissão(%): '))
+    todas_comissao.append(comissao)
 
 
-
-
-
-
-
-vendas = []
-nomes = []
-total_comissao = []
+print('\nRelatório: ')
+for i in range(3):
+    print('-'*40)
+    total.append(todas_vendas[i] * todas_comissao[i] / 100)
+    print(f'{todos_nomes[i]}: valor {total:.2f} ')
+print('-'*40)
 
 for i in range(3):
-    total_nomes = input('\nDigite o nome do vendedor: ')
-    nomes.append(total_nomes)
-    total_vendas = float(input(f'{nomes[i]} informe o total das vendas: '))
-    vendas.append(total_vendas)
-    comissao = float(input(f'Do vendedor {nomes[i]} informe o percentual de comissão(%): '))
-    total_comissao.append(comissao)
+    total_vendas += todas_vendas[i]
+print(f'A soma total de vendas é: {total_vendas}')
 
 
-print(vendas)
-print(nomes)
-    
+for i in range(3):
+    if total[i] > auxiliar:
+        auxiliar = total[i]
+
+        
+
+
+
+print(todos_nomes)
+print(todas_vendas)
+print(todas_comissao)
