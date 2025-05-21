54472464c5d850db43987da770aec751f3847c4a
MiguelP <77172963+Miguelzin635@users.noreply.github.com>
2025-05-21 16:01:46 -0300

    Update ex05.py

--- ex05.py
+++ ex05.py
@@ -3,8 +3,8 @@ todos_nomes = []
 todas_comissao = []
 total_vendas = 0
 soma = 0
-total = []
-auxiliar = 0
+comissao_calculada = []
+maior_valor = 0
 
 for i in range(3):
     nomes = input('Digite o nome do vendedor: ')
@@ -21,24 +21,38 @@ for i in range(3):
 
 print('\nRelatório: ')
 for i in range(3):
-    print('-'*40)
-    total.append(todas_vendas[i] * todas_comissao[i] / 100)
-    print(f'{todos_nomes[i]}: valor {total:.2f} ')
-print('-'*40)
+    print('-'*46)
+    total_receber = (todas_vendas[i] * todas_comissao[i] / 100)
+    print(f'Do vendedor {todos_nomes[i]} o valor a receber é: {total_receber:.2f} ')
+print('-'*46)
 
 for i in range(3):
     total_vendas += todas_vendas[i]
-print(f'A soma total de vendas é: {total_vendas}')
+print(f'A soma total de vendas é: R${total_vendas:.2f}')
 
 
 for i in range(3):
-    if total[i] > auxiliar:
-        auxiliar = total[i]
+    # comissao_calculada são todas as comissões calculadas, ja o maior_valor é a maior comissão
+    valor_comissao = (todas_vendas[i] * todas_comissao[i] / 100)
+    comissao_calculada.append(valor_comissao)
 
+    # Aqui compara com o primeiro indice(o 0 no caso) assumindo o valor, entao o maior_valor vai ser o primeiro valor_copmissao, assim como o menor_valor
+    if i == 0:
+        maior_valor = valor_comissao
+        menor_valor = valor_comissao
+        nome_maior = i
+        nome_menor = i
+
+    elif valor_comissao > maior_valor:
+        maior_valor = valor_comissao
+        # O nome_maior vai guardar dentro desse for o índice (nome do vendedor) cujo qual tem maior comissão(maior_valor)
+        nome_maior = i
+    elif valor_comissao < menor_valor:
+        menor_valor = valor_comissao
+        nome_menor = i
         
+print(f'\nO maior valor a receber é R${maior_valor:.2f} do vendedor {todos_nomes[nome_maior]}')
+print(f'\nO menor valor a receber é R${menor_valor:.2f} do vendedor {todos_nomes[nome_menor]}')
 
 
-
-print(todos_nomes)
-print(todas_vendas)
-print(todas_comissao)
+input('Pressione Enter para fechar o programa...')
